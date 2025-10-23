571d1149cc86881b0ad1e69545674b779548419b
Arkadiy Levin <arkadiyrzn11@gmail.com>
2025-10-23 11:27:08 +0300

    Начал редактирование бота, под то что нужно. На данном этапе поменял декоратор старта, также изменил порядок и название кнопот в меню

--- keyboards.py
+++ keyboards.py
@@ -1,14 +1,15 @@
 from aiogram import types
 
+button1 = types.KeyboardButton(text='Начать тест')
+button2 = types.KeyboardButton(text='Поговорить')
+button3 = types.KeyboardButton(text='Инфо')
+button4 = types.KeyboardButton(text='Очистить')
+button5 = types.KeyboardButton(text='Стоп')
 
-button1 = types.KeyboardButton(text = '/start')
-button2 = types.KeyboardButton(text = '/stop')
-button3 = types.KeyboardButton(text = '/info')
-
-
-keyboard1 =  [
-    [button1, button2, button3],
-    [button2, button3, button1],
+keyboard1 = [
+    [button1],
+    [button2, button3, button4],
+    [button5]
 ]
 
 keyboard = types.ReplyKeyboardMarkup(keyboard=keyboard1, resize_keyboard=True)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+# Импорты и библиотеки
 import asyncio
+from aiogram.fsm.storage.memory import MemoryStorage
 import config
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
@@ -11,13 +13,15 @@ logging.basicConfig(level=logging.INFO)
 
 # Объект бота и диспетчера
 bot = Bot(token=config.token)
-dp = Dispatcher()
+dp = Dispatcher(bot=bot, storage=MemoryStorage())
 
 
-# Декоратор
+# Декоратор.   Приветствие и базовая инфа о боте
 @dp.message(Command(commands=['start']))
 async def start(message: types.Message):
-    await message.answer(f'Привет, {message.from_user.full_name}!', reply_markup=keyboard)
+    await message.answer(
+        f'Привет, {message.from_user.full_name}! \nЭтот бот проверит, репликант ты или нет.\nЖми «Начать тест», если готов узнать результат.\nИли «Поговорить» — если просто хочется пообщаться.',
+        reply_markup=keyboard)
 
 
 @dp.message(Command(commands=['stop']))
@@ -25,16 +29,29 @@ async def start(message: types.Message):
     await message.answer(f'До свидания, {message.from_user.full_name}!')
 
 
+@dp.message(Command(commands=['инфо', 'info']))
 @dp.message(F.text.lower() == 'инфо')
 async def info(message: types.Message):
-    number = random.randint(1, 100)
-    await message.answer(f'Твое число - , {number}!')
+    number = random.randint(0, 100)
+    await message.answer(f'Привет, твоё число: {number}!')
 
 
 @dp.message(F.text)
 async def msg(message: types.Message):
     if 'привет' in message.text.lower():
         await message.reply('И тебе привет!')
+
+#
+# @dp.message(F.text)
+# async def msg(message: types.Message):
+#
+#
+# # if 'привет' in message.text.lower():
+#     await message.reply('И тебе привет!')
+# elif 'как дела' in message.text.lower():
+# await message.reply('Нормально, а у тебя?')
+# else:
+# await message.reply('Не понимаю тебя...')
 
 
 # Запускает бесконечный цикл
